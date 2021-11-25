b65793e41dd4276fd6617c2db0148f999b5aba50
Mateusz Jasiak <mateusz.jasiak.dev@gmail.com>
2021-11-25 16:22:49 +0100

    Adding login integration with Facebook account

--- app/src/main/java/com/mateusz/jasiak/knowmore/MainActivity.java
+++ app/src/main/java/com/mateusz/jasiak/knowmore/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
@@ -16,6 +17,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -26,10 +37,22 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.mateusz.jasiak.knowmore.databinding.ActivityMainBinding;
 
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+import org.json.JSONObject;
+
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
     GoogleSignInClient mGoogleSignInClient;
+
+    //----------------------------------------------------------------------------------------------
+
+    private CallbackManager callbackManager;
+    private LoginButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +75,32 @@ public class MainActivity extends AppCompatActivity {
             startActivityForResult.launch(signInIntent);
         });
 
+        //------------------------------------------------------------------------------------------
+
+        loginButton = findViewById(R.id.login_button);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        loginButton.setPermissions(Arrays.asList("user_friends"));
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d("Demo", "Login Successful!");
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d("Demo", "Login canceled.");
+            }
+
+            @Override
+            public void onError(@NonNull FacebookException e) {
+                Log.d("Demo", "Login error.");
+            }
+        });
+
+        //------------------------------------------------------------------------------------------
 
         Button twoActivity = findViewById(R.id.twoActivity);
 
@@ -122,5 +171,41 @@ public class MainActivity extends AppCompatActivity {
                 break;
             // ...
         }
+    }
+
+    //----------------------------------------------------------------------------------------------
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+
+        GraphRequest graphRequest = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(@Nullable JSONObject jsonObject, @Nullable GraphResponse graphResponse) {
+                Log.d("Demo", jsonObject.toString());
+            }
+        });
+
+        Bundle bundle = new Bundle();
+        bundle.putString("fields", "gender, name, id, first_name, last_name");
+
+        graphRequest.setParameters(bundle);
+        graphRequest.executeAsync();
+    }
+
+    AccessTokenTracker accessTokenTracker = new AccessTokenTracker() {
+        @Override
+        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+            if (currentAccessToken == null) {
+                LoginManager.getInstance().logOut();
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        accessTokenTracker.stopTracking();
     }
 }
